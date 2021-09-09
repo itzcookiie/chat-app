575c09a4a3bb82532ebdac7e5ddbb9ff4d489f37
itzcookiie <mayowaadeniyi@hotmail.com>
2021-09-09 22:13:15 +0100

    feat: Successfully talk to child socket from main socket + some refactoring
    FeelsGoodMan. Managed to route message from client to main socket to child socket when passing down a port from client. In the real world, the client won't know the port number like they do now. They will just connect to a random room or pick what room they want, then the MainSocket will store this information and whenever the client sends a message, they will be automatically routed.

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ import socket
 import sys
 
 string_message = b'Hello from client'
-int_message = (500).to_bytes(4096, sys.byteorder)
+int_message = (3005).to_bytes(4096, sys.byteorder)
 
-address = ('localhost', 3010)
+address = ('localhost', 3000)
 client = socket.create_connection(address)
-client.sendall(string_message)
-message = client.recv(4096)
+client.sendall(int_message)
+message = client.recv(4096).decode('ascii')
 print(message)
--- main.py
+++ main.py
@@ -4,13 +4,41 @@ import sys
 import threading
 
 
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+class IntConverter:
+    @staticmethod
+    def convert_from_bytes_to_dec(int_bytes):
+        return int.from_bytes(int_bytes, byteorder=sys.byteorder)
+
+    @staticmethod
+    def convert_from_dec_to_bytes(number):
+        return number.to_bytes(4096, sys.byteorder)
+
+
+class ChildSocketRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        print(f"Received from client: {data}")
+        # Message sent from MainSocketServer
+        data = self.request.recv(1024).decode('ascii')
+        print(data)
+        print("Message sent by MainSocketServer received!")
+        message = f"ChildSocket: Hey MainSocket! How you doing!"
         cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        response = f"{cur_thread.name}: {message}".encode('ascii')
         self.request.sendall(response)
+
+
+class MainSocketRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request.recv(1024)
+        port = IntConverter.convert_from_bytes_to_dec(data)
+        with socket.socket() as sock:
+            address = ('localhost', port)
+            sock.connect(address)
+            with sock:
+                message = f'MainSocketServer: Hello ChildSocket {port}'.encode('ascii')
+                sock.sendall(message)
+                response = sock.recv(1024).decode('ascii')
+                print(response)
+        self.request.sendall(f'Successfully sent message to port {port}'.encode('ascii'))
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -18,11 +46,11 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 class SocketServer:
-    def __init__(self, port):
+    def __init__(self, port, request_handler):
         self.message = ''
         self.class_name_in_bytes = self.__class__.__name__.encode('utf8')
         self.address = ('localhost', port)
-        self.server = ThreadedTCPServer(self.address, ThreadedTCPRequestHandler)
+        self.server = ThreadedTCPServer(self.address, request_handler)
 
     def start_server(self):
         with self.server:
@@ -40,42 +68,39 @@ class SocketServer:
 
 
 class MainSocketServer(SocketServer):
-    def __init__(self, port):
-        super(MainSocketServer, self).__init__(port)
-
     def handle_message(self, new_socket, addr):
         host, port = addr
         generic_response = self.create_generic_response(port)
-        # child_port = int.from_bytes(self.message, sys.byteorder)
-        # address = ('', child_port)
-        # child_socket = socket.create_connection(address)
-        # child_socket.sendall(generic_response)
         new_socket.sendall(generic_response)
 
 
 # TODO
 # Figure out how to terminate socket from cli
 # Currently have to press CTRL+C and then send another request from the client
+#
 
-# TODO
-# Still working on the task I wrote in currently
-# Try get multithreading to work
-# So try create a socket in a separate thread with a different port number
+def main():
+    socket_servers = []
+    threads = []
+    socket_servers.append(SocketServer(3005, ChildSocketRequestHandler))
+    socket_servers.append(SocketServer(3010, ChildSocketRequestHandler))
+
+    for child_socket in socket_servers:
+        threads.append(threading.Thread(target=child_socket.start_server))
+
+    try:
+        for thread in threads:
+            thread.daemon = True
+            thread.start()
+
+        main_socket = MainSocketServer(3000, MainSocketRequestHandler)
+        main_socket.start_server()
+    finally:
+        for child_socket in socket_servers:
+            child_socket.server.shutdown()
+        main_socket.server.shutdown()
+        print("Closing down servers")
 
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) > 1:
-        if int(args[1]) == 1:
-            c1_socket = SocketServer(3005)
-            c1_thread = threading.Thread(target=c1_socket.start_server())
-            c1_thread.daemon = True
-            c1_thread.start()
-        elif int(args[1]) == 2:
-            c2_socket = SocketServer(3010)
-            c2_thread = threading.Thread(target=c2_socket.start_server())
-            c2_thread.daemon = True
-            c2_thread.start()
-    else:
-        main = MainSocketServer(3000)
-        main.start_server()
+    main()
